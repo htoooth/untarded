791405ebaea60a6bc5164005f1693bddbcc0353e
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2010-08-24 22:07:49 -0500

    more files at once = more awesome up front

--- untarded.rb
+++ untarded.rb
@@ -1,29 +1,33 @@
 # UnTarded by Jason Ormand aka Okor
 
-filename = ARGV[0]
-extension = File.extname(filename.to_s)
+file_names = ARGV
 
 # if no file argument is specified, give usage and exit
-if filename == nil
-  puts 'Example usage: untarded somefile.tar.gz'
-	exit
-end
-
-# if file doesn't exist, give warning and exit
-if File.exist?(filename)==false && File.directory?(filename)==false then
-  puts 'That file does not exist.'
+if !file_names
+  puts '#{$0} usage: untarded [list of files]'
   exit
 end
 
-# where the magic happens
-if extension == '.bz2'
-  `tar -jxvf #{filename}`
-elsif extension == '.gz'
-  `tar -zxvf #{filename}`
-else
-  puts 'I have no idea what to do with this file.'
+action = {}
+action['.bz2'] = 'tar -jxvf'
+action['.gz'] = 'tar -zxvf'
+
+file_names.each do |file_name|
+  extension = File.extname(file_name.to_s)
+
+  # if file doesn't exist, give warning and exit
+  if !File.exist?(file_name) && !File.directory?(file_name) then
+    puts 'That file does not exist.'
+    next
+  end
+
+  if !action[extension]
+    puts 'I have no idea what to do with #{file_name}.'
+    next
+  end
+
+  # where the magic happens
+  system("#{action[extension]} #{file_name}")
 end
-
-
 
 
